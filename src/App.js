6c4d8f030f08c40b6f0056c5c0a48ca808972bf5
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-02-25 02:08:10 -0600

    CRA BP and Reset UP Done for CRA This commit: ~~About to Start AJAX ~~This Way My Dependicies are normal for me
    Because:
    -{I changed to CRA rather than base build}

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Pet from './components/Pet';
+import SearchParams from './components/SearchParams';
+
+const App = () => {
+	return (
+		<div>
+			<h1>Adopt Me!</h1>
+			<SearchParams />
+		</div>
+	);
+};
+
+export default App;
